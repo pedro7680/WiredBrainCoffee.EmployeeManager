8fdae99ba8e9bb1c3b187403ba9ab71f04bf51d7
peter <peter@VICTUS-LAPTOP>
2023-06-29 21:55:13 +0100

    About page was added and tested.

---
+++ WiredBrainCoffee.EmployeeManager/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiredBrainCoffee.EmployeeManager.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
